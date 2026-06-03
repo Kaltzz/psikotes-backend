6bc1be23ad42cefeb271e6981d5f0a3392573965
korokoro22 <andimuhammadrezkym@gmail.com>
2026-06-03 11:02:34 +0800

    fixing appts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express()
 
 
 app.use(cors({
-  // origin: 'http://localhost:3000', //jika local
+  // origin: 'http://localhost:3000', //jika local/
   origin: 'https://psikotes-frontend.vercel.app',
   credentials: true,
 
